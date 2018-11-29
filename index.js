e343eaafaa1b685d7e6cd88d270ae99d1b46a631
jumanja <jumanja@gmail.com>
2018-11-29 16:41:43 -0500

    Validating  method and adding JSON response for POST

--- index.js
+++ index.js
@@ -4,8 +4,21 @@
 
 //Dependencies
 var http = require('http');
+var url = require('url');
+
 var server = http.createServer(function(req,res){
-  res.end('Hello REST World\n');
+
+  var parsed   = url.parse(req.url,true);
+  var path     = parsed.pathname.toLowerCase();
+  var method   = req.method.toLowerCase();
+
+  path = (path.charAt(path.length-1) == "\\" ? path.charAt(path.length-1) : path);
+
+  if(path == "/hello" && method == "post"){
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200);
+    res.end('{"message": "Hello REST World using method POST"}');
+  };
 })
 
 server.listen(7373,function(){
